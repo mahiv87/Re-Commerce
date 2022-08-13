5cf3fd3d7380a3ee393caf31a3b285004e8c5368
Marcus Herrera <maherreraiv1987@gmail.com>
2022-08-13 12:04:04 -0600

    debugged CategoryMenu

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -11,8 +11,16 @@ import {
 	TOGGLE_CART
 } from './actions';
 
+const initialState = {
+	products: [],
+	categories: [],
+	currentCategory: [],
+	cart: [],
+	cartOpen: false
+};
+
 // The reducer is a function that accepts the current state and an action. It returns a new state based on that action.
-export default function reducer(state, action) {
+export default function reducer(state = initialState, action) {
 	switch (action.type) {
 		// Returns a copy of state with an update products array. We use the action.products property and spread it's contents into the new array.
 		case UPDATE_PRODUCTS:
